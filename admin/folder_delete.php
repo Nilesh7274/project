d88772fa9642c78a173709ad5f1402f6f3f95478
Nilesh7274 <116290822+Nilesh7274@users.noreply.github.com>
2022-10-28 12:53:20 +0530

    d

--- admin/folder_delete.php
+++ admin/folder_delete.php
@@ -3,12 +3,13 @@ include 'conn.php';
 
 $id=$_REQUEST['id'];
 $q="delete from notes where id=".$id;
-//$q="delete from notes where pr_id=".$id;
+$q2="delete from notes where pr_id=".$id;
 //$q="DELETE FROM notes WHERE id=".$id." and pr_id=".$id;
 //$q1"delete from notes where pr_id=".$id ;
 //DELETE FROM `notes` WHERE id=1 and pr_id=
 echo $q;
 $res=mysqli_query($conn,$q);
+$res2=mysqli_query($conn,$q2);
 //$res1=mysqli_query($conn,$q1);
 if(mysqli_affected_rows($conn)>0)
 {
