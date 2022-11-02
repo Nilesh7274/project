eac3e82cc16853888bfdfb7c2e63bda2954b4681
pratik <pratiksulakshane54@gmail.com>
2022-11-02 18:01:08 +0530

    oo

--- header.php
+++ header.php
@@ -68,7 +68,7 @@ if(!isset($_SESSION['student']))
  <header id="header" class="header fixed-top d-flex align-items-center">
 
 <div class="d-flex align-items-center justify-content-between">
-  <a href="index.html" class="logo d-flex align-items-center">
+  <a href="#" class="logo d-flex align-items-center">
     <img src="assets/img/cloud.png" alt="">
     <span class="d-none d-lg-block">CloudClass</span>
   </a>
